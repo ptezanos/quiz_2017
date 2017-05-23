7e67655b0565610db9e6abe234620f6a8dbbdb09
ptezanos <pablo.tezanos@gmail.com>
2017-05-23 21:53:14 +0200

    Actualización de models.index.js

--- models/index.js
+++ models/index.js
@@ -22,9 +22,18 @@ if (!process.env.DATABASE_URL) {
 var sequelize = new Sequelize(url, {storage: storage});
 
 
-
-// Importar la definicion de la tabla Quiz de quiz.js
+//Importar la definicion de la tabla Quiz de quiz.js
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
+
+//Crear tablas
+sequelize.sync()
+.then(function () {
+	console.log('Bases de datos creadas con éxito');
+})
+.catch(function (error) {
+	console.log("error creando las tablas de la BBDD:", error);
+	process.exit(1);
+});
 
 
 exports.Quiz = Quiz; // exportar definición de tabla Quiz
